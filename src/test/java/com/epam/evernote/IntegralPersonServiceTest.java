bad2bfbe9793a0c85dc28c3b6de50b47f359bfea
Sergei Titov <Sergei_Titov@epam.com>
2017-12-06 19:59:15 +0300

    re-factored: renamed classes, added comments

---
+++ src/test/java/com/epam/evernote/IntegralPersonServiceTest.java
@@ -0,0 +1,83 @@
+package com.epam.evernote;
+
+import com.epam.evernote.config.IntegralPersonServiceConfig;
+import org.junit.*;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * Integral test for Person Service.
+ */
+public class IntegralPersonServiceTest {
+    private static AnnotationConfigApplicationContext daoTestContext;
+    private static PersonService personService;
+
+    /**
+     * Database SetUp
+     */
+    @BeforeClass
+    public static void setUpDB() {
+        daoTestContext = new AnnotationConfigApplicationContext(IntegralPersonServiceConfig.class);
+        personService = daoTestContext.getBean(PersonService.class);
+    }
+
+    /**
+     * Test with series of operations within Person
+     */
+    @Test
+    public void TestPersonService() {
+
+        final String hardName = "Some name hardly to meet";
+        final String easyName = "Sergei";
+
+        // create one person with hard name
+        personService.savePerson(Person.create(hardName, true));
+        Assert.assertEquals(personService.getAllPersons().size(), 1);
+
+        // create another person with easy name
+        Person person = Person.create(easyName, true);
+        personService.savePerson(person);
+        Assert.assertEquals(personService.getAllPersons().size(), 2);
+
+        // find by id
+        final long id = person.getId();
+        person = personService.getPersonById(2/*id*/);
+        Assert.assertEquals(person.getName(), easyName);
+
+        // find by name
+        List<Person> persons = personService.getPersonsByName(hardName);
+        Assert.assertEquals(persons.size(), 1);
+        person = persons.get(0);
+        Assert.assertEquals(person.getName(), hardName);
+
+        // update name
+        final String newName = "Titov";
+        personService.updateName(2/*id*/, newName);
+
+        // check if number of persons didn't change
+        persons = personService.getAllPersons();
+        int count = persons.size();
+        Assert.assertEquals(personService.getAllPersons().size(), count);
+
+        // check if person was really renamed
+        Assert.assertEquals(personService.getPersonById(2/*id*/).getName(), newName);
+
+        // delete person
+        personService.deletePerson(2/*id*/);
+
+        // check if person was really deleted
+        Assert.assertNull(personService.getPersonById(2/*id*/));
+    }
+
+    /**
+     * Closes DataBase connection
+     * @throws SQLException if something went wrong with DB connection
+     */
+    @AfterClass
+    public static void tearDown() throws SQLException {
+        daoTestContext.getBean(DataSource.class).getConnection().close();
+    }
+}
--- src/test/java/com/epam/evernote/config/IntegralPersonServiceConfig.java
+++ src/test/java/com/epam/evernote/config/IntegralPersonServiceConfig.java
@@ -4,12 +4,15 @@ import com.epam.evernote.JdbcTemplatePersonDao;
 import com.epam.evernote.PersonDao;
 import com.epam.evernote.PersonService;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
 import javax.sql.DataSource;
 
-public class IntegralServiceDaoConfig {
+public class IntegralPersonServiceConfig {
+
+    private static EmbeddedDatabase db = null;
 
     @Bean
     public PersonService personService() {
@@ -23,9 +26,13 @@ public class IntegralServiceDaoConfig {
 
     @Bean
     public DataSource h2DataSource() {
-        return new EmbeddedDatabaseBuilder()
-                .setType(EmbeddedDatabaseType.H2)
-                .addScript("createPersonTable.sql") //script to create person table
-                .build();
+        if (null == db) {
+            db = new EmbeddedDatabaseBuilder()
+                    .setType(EmbeddedDatabaseType.H2)
+                    .addScript("createPersonTable.sql") //script to create person table
+                    .build();
+        }
+
+        return db;
     }
 }
