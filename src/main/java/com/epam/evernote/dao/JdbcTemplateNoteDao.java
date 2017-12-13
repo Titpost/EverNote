eaa92bb0a8da256ea0cf49aa6d1817209c00336d
Titov Sergei <quosapv@yandex.ru>
2017-12-13 23:38:57 +0300

    removed unneeded ("personTemplateRepo") from @Repository

--- src/main/java/com/epam/evernote/dao/JdbcTemplateNoteDao.java
+++ src/main/java/com/epam/evernote/dao/JdbcTemplateNoteDao.java
@@ -17,7 +17,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Repository("noteTemplateRepo")
+@Repository
 public class JdbcTemplateNoteDao implements NoteDao {
 
     @Autowired
--- src/main/java/com/epam/evernote/dao/JdbcTemplatePadDao.java
+++ src/main/java/com/epam/evernote/dao/JdbcTemplatePadDao.java
@@ -17,7 +17,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Repository("padTemplateRepo")
+@Repository
 public class JdbcTemplatePadDao implements PadDao {
 
     @Autowired
