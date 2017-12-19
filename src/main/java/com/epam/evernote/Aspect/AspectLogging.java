6ae6d1144716e6623e0e2b9dbabaa575556a078d
pashkevich <pavelandreevsk@gmail.com>
2017-12-20 00:04:52 +0300

    Added AOP Logging

--- src/main/java/com/epam/evernote/Aspect/AspectLogging.java
+++ src/main/java/com/epam/evernote/Aspect/AspectLogging.java
@@ -1,8 +1,14 @@
 package com.epam.evernote.Aspect;
 
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.LocalTime;
 
 
 @Aspect
@@ -10,4 +16,18 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class AspectLogging {
 
+    @Pointcut("within(com.epam.evernote.service.Implementations.*)")
+    public void interceptServiceMethod() {
+    }
+
+    @Around("interceptServiceMethod()")
+    public Object getAll(ProceedingJoinPoint joinPoint) throws Throwable {
+            LocalTime start = LocalTime.now();
+            log.info("{} is starting at {}", joinPoint.getSignature(), start);
+            Object proceed = joinPoint.proceed();
+            log.info("{} returned {} after {}", joinPoint.getSignature(), proceed,
+                    Duration.between(LocalTime.now(), start));
+        return proceed;
+    }
 }
+
